93fddb383fb162361c69d74bcb29be2962beeecf
Aviraj-create <81038098+Aviraj-create@users.noreply.github.com>
2023-08-11 23:43:55 +0530

    final deploy before added features

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,13 +1,65 @@
-const Page = async () => {
+"use server"
+import CommunityCard from '@/components/cards/CommunityCard';
+import UserCard from '@/components/cards/UserCard';
+import PostThread from '@/components/forms/PostThread';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import ThreadsTab from '@/components/shared/ThreadsTab';
+
+import { profileTabs } from '@/constants';
+import { fetchCommunities } from '@/lib/actions/community.actions';
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs';
+
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
+
+
+async function Page() {
+    const user = await currentUser();
+    if (!user) return null;
+    const userInfo = await fetchUser(user.id);
+    if (!userInfo?.onboarded) redirect('/onboarding');
+
+    //fetch users
+    const result = await fetchCommunities({
+
+        searchString: '',
+        pageNumber: 1,
+        pageSize: 25
+    })
+
     return (
         <section>
             <h1 className="head-text mb-10">
-                Communities
+                Search
             </h1>
+            {/* SearchBar */}
+
+            <div className='mt-14 flex flex-col gap-9'>
+                {result.communities.length === 0 ? (
+                    <p className='no-result'> Nocommunitiess</p>
+                ) : (
+                    <>
+                        {result.communities.map((community) => (
+                            <CommunityCard
+                                key={community.id}
+                                id={community.id}
+                                name={community.name}
+                                username={community.username}
+                                imgUrl={community.image}
+                                bio={community.bio}
+                                members={community.members}
+
+                            />
+                        ))}
+
+                    </>
+                )}
+            </div>
         </section>
     )
 
 
 }
 
-export default Page;
+export default Page; 
